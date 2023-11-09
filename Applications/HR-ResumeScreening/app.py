0a4b3a2bb62f6d9db0370bea9f0ed687c1bbb1a1
suvosmac <suvo.bhattacharya@gmail.com>
2023-11-09 15:37:52 +0530

    HR Resume screening application coding checkin - WIP - issue with similarity_search_with_score function in Pinecone index

--- Applications/HR-ResumeScreening/app.py
+++ Applications/HR-ResumeScreening/app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import os
 import uuid
+from utils import *
 
 # creating session variables
 if 'unique_id' not in st.session_state:
@@ -28,22 +30,53 @@ def main():
         with st.spinner("Wait for it..."):
 
             st.write("our process")
-            # creating a unique id , which we can use to query and get only the user uploaded documents from Pinecone vector store
+            # creating a unique id , which we can use to query and
+            # get only the user uploaded documents from Pinecone vector store
             st.session_state['unique_id'] = uuid.uuid4().hex
-            st.write(st.session_state['unique_id'])
-
-            # create a uniqueID, so that we can use to query and get only the user
-            # uploaded documents from PINECONE vector store
 
             # create a documents list out of all the user uploaded pdf files
+            docs = create_docs(pdf, st.session_state['unique_id'])
 
             # Displaying the count of resumes that have been uploaded
+            st.write("Number of Resumes Uploaded - "+str(len(docs)))
 
             # create embeddings instance
+            embeddings = create_embeddings_load_data()
 
             # push data to pinecone
+            push_to_pinecone(os.getenv("PINECONE_API_KEY"),
+                             os.getenv("PINECONE_ENVIRONMENT"),
+                             "resumes",
+                             embeddings, docs)
 
             # fetch relevant data from pinecone
+            relevant_docs = similar_docs(job_description, document_count,
+                                         os.getenv("PINECONE_API_KEY"),
+                                         os.getenv("PINECONE_ENVIRONMENT"),
+                                         "resumes", embeddings, st.session_state['unique_id'])
+
+            st.write(relevant_docs)
+
+            # Introducing a line separator
+            st.write(":heavy_minus_sign:" * 30)
+
+            # For each item in relavant docs - we are displaying some info of it on the UI
+            for item in range(len(relevant_docs)):
+
+                st.subheader("👉 "+str(item+1))
+
+                # Displaying Filepath
+                st.write("**File** : "+relevant_docs[item][0].metadata['name'])
+
+                # Introducing Expander feature
+                with st.expander('Show me 👀'):
+                    st.info("**Match Score** : "+str(relevant_docs[item][1]))
+                    # st.write("***"+relavant_docs[item][0].page_content)
+
+                    # Gets the summary of the current item using 'get_summary'
+                    # function that we have created which uses LLM & Langchain chain
+                    summary = get_summary(relevant_docs[item][0])
+                    st.write("**Summary** : "+summary)
 
         st.success("Hope I was able to save your time❤️")
 
--- Applications/HR-ResumeScreening/utils.py
+++ Applications/HR-ResumeScreening/utils.py
@@ -1,9 +1,14 @@
+import os
 from pypdf import PdfReader
-from langchain.schema import Document
+import pinecone
+from langchain.llms import OpenAI
+from langchain.chains.summarize import load_summarize_chain
+from langchain.vectorstores import Pinecone
+from langchain.schema import Document, embeddings
+from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+
 
 # Extract information from the PDF file
-
-
 def get_pdf_text(pdf_doc):
 
     text = ""
@@ -24,8 +29,62 @@ def create_docs(user_pdf_list, unique_id):
         # adding item to our list and adding metadata
         docs.append(Document(
             page_content=chunks,
-            metadata={"name": filename.name, "id": filename.id, "type": filename.type,
+            metadata={"name": filename.name, "id": filename.file_id, "type": filename.type,
                       "size": filename.size, "unique_id": unique_id}
         ))
 
     return docs
+
+
+# create embeddings instance
+def create_embeddings_load_data():
+    embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-V2")
+    return embeddings
+
+
+# function to push data to vector store(Pinecone)
+def push_to_pinecone(pinecone_apiKey, pinecone_environment, pinecone_index_name, embeddings, docs):
+
+    pinecone.init(api_key=pinecone_apiKey,
+                  environment=pinecone_environment)
+
+    print("data pushed to pinecone")
+    Pinecone.from_documents(docs, embeddings, index_name=pinecone_index_name)
+
+
+# function to pull data from vecrtor store
+def pull_from_pinecone(pinecone_apiKey, pinecone_environment,
+                       pinecone_index_name, embeddings):
+
+    pinecone.init(api_key=pinecone_apiKey,
+                  environment=pinecone_environment)
+
+    index = Pinecone.from_existing_index(pinecone_index_name, embeddings)
+    return index
+
+
+# function to help us get relevant documents
+# from vector store based on user input
+def similar_docs(query, number_of_resumes, pinecone_apiKey,
+                 pinecone_environment, pinecone_index_name,
+                 embeddings, unique_id):
+
+    pinecone.init(api_key=pinecone_apiKey,
+                  environment=pinecone_environment)
+
+    index = pull_from_pinecone(
+        pinecone_apiKey, pinecone_environment, pinecone_index_name, embeddings)
+
+    similar_docs = index.similarity_search_with_score(
+        query, int(number_of_resumes), {"unique_id": unique_id})
+    return similar_docs
+
+# Function which helps us to get the summary of a document
+
+
+def get_summary(current_doc):
+    llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"), temperature=0)
+    chain = load_summarize_chain(llm, chain_type="map_reduce")
+    summary = chain.run([current_doc])
+
+    return summary
