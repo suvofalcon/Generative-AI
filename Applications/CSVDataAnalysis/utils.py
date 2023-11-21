868c5b63e62966c36d357e9e7ec3564ca0fe2221
suvosmac <suvo.bhattacharya@gmail.com>
2023-11-21 19:40:50 +0530

    updated langchain and hence had to change the code import

--- Applications/CSVDataAnalysis/utils.py
+++ Applications/CSVDataAnalysis/utils.py
@@ -1,7 +1,6 @@
-from langchain.agents import create_pandas_dataframe_agent
 import pandas as pd
 from langchain.llms import OpenAI
-
+from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 
 def query_agent(data, query):
     # Parse the CSV file and create a pandas dataframe from its contents
@@ -14,7 +13,7 @@ def query_agent(data, query):
     agent = create_pandas_dataframe_agent(llm, df, verbose=True)
 
     '''
-    Python REPL: A Python shell used to evaluating and executing Python commands. 
+    Python REPL: A Python shell used to evaluating and executing Python commands.
     It takes python code as input and outputs the result. The input python code can be generated from another tool in the LangChain
     '''
     return agent.run(query)  # returning the response that is coming for our query
