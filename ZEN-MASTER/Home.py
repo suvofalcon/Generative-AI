353830daafe247acc939f5779b38e3e946f7310b
suvosmac <suvo.bhattacharya@gmail.com>
2023-11-29 20:06:45 +0530

    ZEN MASTER APP - Consolidation of all GenAI applications through a streamlit app

---
+++ ZEN-MASTER/Home.py
@@ -0,0 +1,55 @@
+# Library imports
+
+import streamlit as st
+import os
+
+st.set_page_config(page_title="Generative AI powered Applications", page_icon=":robot:")
+st.header("Proof of Concepts - Possibilities with Generative AI", divider=True)
+
+st.subheader("Following Technologies are used - ")
+
+st.markdown('''
+
+    - [Streamlit](https://streamlit.io)
+    - [LangChain](https://python.langchain.com) Abstraction APIs
+    - [OpenAI](https://platform.openai.com/docs/models) LLM Model
+    - [LLama2 7B Quantized Model](https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/tree/main) LLM Model
+    - [Opensource Embedding (all-MiniLM-L6-V2)](https://huggingface.co/sentence-transformers/all-MiniLM-L6-V2) Sentence Transformer Model
+    - [Vector Store - FAISS](https://ai.meta.com/tools/faiss/) - Facebook AI Similarity Search
+
+''')
+
+st.markdown('''
+
+### Application Details 
+
+#### 1 - Code Documentation Generator
+
+This uses OpenAI Davinci Model (v3.5) to generate Detailed Documentation of Source code. This includes 
+- Identification of Programming Language.
+- Creating a summary of the code functionality
+- Detailed Step by Step Explanation of what is happening in each line of the code
+
+This may be useful for teams during Transition to generate documentation for application code, in cases where adequation documentaion is unavailable
+
+This has been currently tested for Python , Java, C/C++ and COBOL only, but may work for other additional programming languages also.
+
+ ---
+
+#### 2 - Work with Knowledge Documents
+
+This has two implementations
+- Using OpenAI Embedding and OpenAI Davinci Model   
+- Using open source embedding Model from HuggingFace and LLama2 7B Quantized Model
+- This also uses FAISS as the vector store for Similarity Search Query
+
+This builds a natural language based interation using the power of Generative AI on custom documents , such has KEDB, SOPs, Ticket Descriptions and even large text based logs and traces.
+
+ ---
+
+ #### 3 - Data Analysis using LLM
+
+ This uses LLM to put a natural language based query interface on a dataset. This has been currently implemented using CSV files as the data source, but can directly work with database tables and structures - Both relational and NoSQL.
+
+---
+''')
---
+++ ZEN-MASTER/pages/1_Code_Documentation_Generator.py
@@ -0,0 +1,127 @@
+# Library Imports
+
+import streamlit as st
+import os
+from io import StringIO
+
+from langchain.llms.openai import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.chains import SequentialChain
+
+# Read the OpenAI API Key from environment
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+
+# Read the source file - as bytes data
+def read_source_file(file):
+    
+    if file is not None:
+        code_data = file.read()
+    
+    return code_data
+
+# Read the content of source file - to preserve the code format and style
+def read_code_file(file):
+
+    if file is not None:
+        if file.type == 'text/plain':
+
+            stringio = StringIO(file.getvalue().decode('utf-8'))
+            code_data = stringio.read()
+
+    return code_data
+
+
+# Initialize OpenAI
+def initialize_llm():
+
+    llm = OpenAI(engine='text-davinci-003', temperature=1,
+                 max_tokens = 512,
+                 openai_api_key = OPENAI_API_KEY)
+
+    return llm
+
+# Design the code generation prompt
+def design_doc_generation_prompt(code_data):
+
+    # Prompts design
+    lang_identify = """Please identify the programming language given in the code :\n{code_data}
+    
+    YOUR RESPONSE:
+    """
+
+    lang_identify_template = PromptTemplate(
+        input_variables=['code_data'],
+        template = lang_identify
+    )
+    
+    code_summary = """Please give a brief summary description of the code below :\n{code_data}
+
+    YOUR RESPONSE:
+    """
+
+    code_summary_template = PromptTemplate(
+        input_variables=['code_data'],
+        template = code_summary
+    )
+
+    code_steps = """Please explain the working of the code below in a detailed step by step manner :\n{code_data}
+
+    YOUR RESPONSE:
+    """
+
+    code_steps_template = PromptTemplate(
+        input_variables = ['code_data'],
+        template = code_steps
+    )
+
+    return lang_identify_template, code_summary_template, code_steps_template
+
+
+# Construct the required chains and execute the prompts
+def construct_execute_chain(model, lang_template, summary_template, steps_template):
+
+    # construct the chains for sequential execution
+    language_chain = LLMChain(llm=model, prompt=lang_template, output_key="lang")
+    summary_chain = LLMChain(llm=model, prompt=summary_template, output_key="summary")
+    steps_chain = LLMChain(llm=model, prompt=steps_template, output_key='steps')
+
+    # Build the final sequential chain
+    final_chain = SequentialChain(chains=[language_chain, summary_chain, steps_chain],
+                                  input_variables=['code_data', 'code_data', 'code_data'],
+                                  output_variables=['lang', 'summary', 'steps'],
+                                  verbose=True)
+
+    response = final_chain(code_data)
+
+    return response
+
+
+st.title("Code Documentation Generator")
+st.subheader("Generate Detailed Documentation for Python, Java, C/C++ and COBOL files")
+st.markdown(''' --- ''')
+# Build file uploader
+source_file = st.file_uploader("Upload your Source File", type=["txt"], accept_multiple_files=False)
+
+generate_btn = st.button("Generate Documentation")
+
+if generate_btn:
+    
+    # read the source file contents
+    code_contents = read_code_file()
+
+    # get the prompts 
+    language, summary, detailed_steps = design_doc_generation_prompt(code_data=code_contents)
+
+    # initialize the llm
+    llm = initialize_llm()
+
+    # execute the prompts on llm
+    response = construct_execute_chain(model=llm, lang_template=language, 
+                                       summary_template=summary, steps_template=detailed_steps)
+
+    st.write()
+
+st.markdown(''' --- ''')
+st.success("Done!!")
