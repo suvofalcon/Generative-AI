16500be640ffd92234d61a690c4d2e3c8b3a2359
suvosmac <suvo.bhattacharya@gmail.com>
2023-12-07 19:26:11 +0530

    Query Enterprise Documents using - Pinecone, GooglePaLM and GooglePaLM Embeddings

---
+++ ZEN-MASTER/pages/6_Query_Enterprise_Documents_- GooglePaLM.py
@@ -0,0 +1,138 @@
+# Library Imports ###########################################################
+import streamlit as st
+import os
+from PyPDF2 import PdfReader
+import pinecone
+from langchain.prompts import PromptTemplate
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import GooglePalmEmbeddings
+from langchain.vectorstores.pinecone import Pinecone
+
+
+# Some important configurations to be read ##################################
+pinecone_key = os.getenv("PINECONE_API_KEY")
+pinecone_env = os.getenv("PINECONE_ENVIRONMENT")
+
+# some global session variable
+st.session_state['vectorStore'] = ''
+
+# define the custom prompt template
+custom_prompt_template = """Use the following pieces of information to answer
+user's question. In case you dont know the answer, just say you dont know
+the answer, dont try to make up an answer.
+
+Context: {context}
+Question: {question}
+
+Only return the helpful answer below and nothing else.
+
+Helpful answer:
+"""
+
+# Functions with implementation #############################################
+
+# Function to get pdf text from all pdf docs
+
+
+def get_pdf_text(pdf_docs):
+
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader()
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+    return text
+
+# Create Chunks from PDF Text
+
+
+def create_chunks_from_text(pdf_text):
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators="\n",
+        chunk_size=512,
+        chunk_overlap=20,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(pdf_text)
+    return chunks
+
+# Create Vector Store using Embeddings for the pdf text
+
+
+def create_vectorStore_from_chunks(chunks, indexName):
+
+    # Initialize Pinecone vector Store
+    pinecone.init(
+        api_key=pinecone_key,
+        environment=pinecone_env
+    )
+
+    # Initialize embedings
+    embeddings = GooglePalmEmbeddings()
+
+    # create the vector store
+    vector_store = Pinecone.from_texts(
+        [t.page_content for t in chunks], embeddings, index_name=indexName)
+    st.session_state['vectorStore'] = vector_store
+
+    # if an index is already existing , we can load like this
+    # vector_store = Pinecone.from_existing_index(indexName, embeddings)
+    return vector_store
+
+# Function to set the custom prompt
+
+
+def set_custom_prompt():
+
+    prompt = PromptTemplate(
+        template=custom_prompt_template,
+        input_variables=['context', 'question']
+    )
+    return prompt
+
+
+# UI Begins from here ########################################################
+st.subheader("Chat with Google PaLM on a knowledge base :books:")
+st.markdown(''' --- ''')
+
+# Create tabs
+tab_titles = ['Vector Store(PaLM Embeddings + Pinecone)', 'Query Google PaLM']
+tabs = st.tabs(tab_titles)
+
+with tabs[0]:
+
+    st.subheader(
+        "Create Vector Store on your KEDB using PaLM Embeddings in Pinecone")
+    docs = st.file_uploader("Upload your Documents", type=[
+                            'pdf'], accept_multiple_files=True)
+
+    process_btn = st.button("Process Vector Store", key="process_btn")
+
+with tabs[1]:
+
+    st.subheader("Query your KEDB using Google PaLM")
+    query_area = st.text_area("Enter your Query")
+
+    response_btn = st.button("Get Response", key="response_local")
+
+if process_btn:
+
+    with st.spinner("Creating Pinecone Vector Store..."):
+
+        # Get the texts from PDFs uploaded
+        pdf_texts = get_pdf_text(pdf_docs=docs)
+        chunks = create_chunks_from_text(pdf_text=pdf_texts)
+
+        # create the vector store
+        create_vectorStore_from_chunks(chunks, "kedb")
+        st.success("Done...")
+
+if response_btn:
+
+    with st.spinner("Getting response from Google PaLM..."):
+
+        if st.session_state['vectorStore'] is not None:
+
+            st.success("Done...")
